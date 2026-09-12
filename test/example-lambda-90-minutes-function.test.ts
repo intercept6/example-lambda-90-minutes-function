3b594ec2288ea2f76f3058a4e8d914c3c5b2e462
intercept6 <4459220+intercept6@users.noreply.github.com>
2026-09-12 19:01:21 +0900

    chore: scaffold CDK TypeScript app

---
+++ test/example-lambda-90-minutes-function.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExampleLambda90MinutesFunction from '../lib/example-lambda-90-minutes-function-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/example-lambda-90-minutes-function-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExampleLambda90MinutesFunction.ExampleLambda90MinutesFunctionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
